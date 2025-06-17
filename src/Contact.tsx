b8ffb8e004c4a6e29a185231f541a4c5383b335e
âtghoshgit config --global user.email ‘satn620502@dal.ca <tn620502@dal.ca>
2025-06-17 20:57:52 -0300

    Clean Up

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,11 +1,10 @@
 function Contact() {
 
-
   return (
     <>
       <div className="p-10 bg-neutral-100 w-99vw text-black"> 
         <p>Feel free to reach out. You can contact me by sending an email to tanishkaghosh1122@gmail.com.</p>
-        <p>The repository for this portfolio is public and can be found at: <a href="#">Github Link </a></p>
+        <p>The repository for this portfolio is public and can be found at: <a href="https://github.com/Tanishka-G/my-react-app" target="_blank">Github Link </a></p>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import Start from './Start.tsx'
 import Projects from './Projects.tsx'
 import Gallery from './Gallery.tsx'
