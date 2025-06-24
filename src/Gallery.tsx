fc392c7627515b277aed494ec492c8732dfeb538
âtghoshgit config --global user.email ‘satn620502@dal.ca <tn620502@dal.ca>
2025-06-23 22:57:43 -0300

    Clicking right or left arrow to limit redos  the image displayed (instead of disabling button)

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -4,25 +4,26 @@ import pic4 from './assets/pic-1.jpg'
 import pic3 from './assets/pic-2.jpg'
 import pic1 from './assets/pic-3.jpg'
 import pic2 from './assets/pic-4.jpg'
+import pic5 from './assets/pic-5.jpg'
 import left from './assets/left-arrow.png'
 import right from './assets/right-arrow.png'
 
 
 function Gallery(){
     const [picIndex, setPicIndex] = useState(0);
-    const imageArray = [pic1, pic2, pic3, pic4];
+    const imageArray = [pic1, pic2, pic3, pic4, pic5];
 
     return(
         <>
         <div className="h-screen">
-            <p className="text-center text-7xl font-black pt-10 pb-10">Some Pictures</p>
+            <p className="text-center text-5xl pt-10 pb-10">Some Pictures</p>
             <div className="gallery">
-                <button className="arrow" onClick={() => setPicIndex((prev) => prev - 1)} disabled= {picIndex > 0 ? false : true}>
-                    <img src={left} alt="left arrow" style={{opacity: picIndex > 0 ? 1 : 0.5}} className="h-32 w-32"/>
+                <button className="arrow" onClick={() => setPicIndex((prev) => (prev - 1 + imageArray.length) % imageArray.length)}>
+                    <img src={left} alt="left arrow" className="h-32 w-32 m-5 p-3"/>
                 </button>
                 <img className="gallery-pic" alt="current gallery image" src={imageArray[picIndex]} />
-                <button  className="arrow" onClick={() => setPicIndex((prev) => prev + 1)} disabled= {picIndex < imageArray.length - 1 ? false : true}>
-                    <img src={right} alt="right arrow" style={{opacity: picIndex < imageArray.length - 1 ? 1 : 0.5}} className="h-32 w-32"/>
+                <button  className="arrow" onClick={() => setPicIndex((prev) => (prev + 1) % imageArray.length)}>
+                    <img src={right} alt="right arrow" className="h-32 w-32 m-5 p-3"/>
                 </button>
             </div>
         </div>
